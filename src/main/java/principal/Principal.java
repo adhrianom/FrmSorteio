17061b8ccfa79e445951ddbbf8c059231d5d3649
10725114655 <10725114655@UNS07DT99843.animaedu.intranet>
2025-05-12 11:36:49 -0300

    Exemplo 3 JFrame

---
+++ src/main/java/principal/Principal.java
@@ -0,0 +1,9 @@
+package principal;
+import view.FrmSorteio;
+public class Principal {
+
+    public static void main(String[] args) {
+        FrmSorteio sorteio = new FrmSorteio();
+        sorteio.setVisible(true);
+    }
+}
